faa30c0108104d9611dc5d6d5de31854b97ee2d3
isnendyan <isnendyankp@gmail.com>
2023-02-23 16:32:26 +0700

    add show & hide function

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+// Loading Spinner Shown
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
 
 // Show New Quote
 function newQuote() {
@@ -50,4 +63,5 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 //  On Load
-getQuotes();
+// getQuotes();
+loading();
